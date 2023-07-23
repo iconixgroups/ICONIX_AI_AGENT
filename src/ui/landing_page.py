b510eb5560b89e3963034ed8c441b1e4d1691cf6
ICONIX <101103144+iconixgroups@users.noreply.github.com>
2023-07-23 10:58:00 +0400

    Update landing_page.py
    Prefix ```python and Suffix ``` Removed

--- src/ui/landing_page.py
+++ src/ui/landing_page.py
@@ -1,4 +1,3 @@
-```python
 from flask import Flask, render_template, request, redirect, url_for
 from src.user_management import createUser, loginUser
 
@@ -25,5 +24,5 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
-```
+
 This Python code uses Flask, a web framework, to create the landing page for the ICONIX AI Agent web application. It defines routes for the landing page, signup, and login. The signup and login routes use POST methods to receive form data from the user, and call the createUser and loginUser functions from the user_management module to handle user creation and login. After successful signup or login, the user is redirected back to the landing page with a success message.
